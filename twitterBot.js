08bdd8f4c376fc13d44412f43b42fe3b96244a04
Curtis Turk <curtis.c.turk@outlook.com>
2023-01-23 20:59:12 +0000

    Trying with Twitter-v2

--- twitterBot.js
+++ twitterBot.js
@@ -1,10 +1,11 @@
-import puppeteer from "puppeteer";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-let browser = null;
-let page = null;
+import Twitter from "twitter-v2";
 
-(async () => {
-  browser = await puppeteer.launch({ headless: false });
-})();
+const T = new Twitter({
+  consumer_key: process.env.API_KEY,
+  consumer_secret: process.env.API_KEY_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+});
